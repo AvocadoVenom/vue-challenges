6be50ef733850d9dd4377ed2e83c7cbda1cb4187
Adrien <adrien@mbp-adrien.home>
2019-11-24 12:36:56 +0100

    Add navigation menu

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,26 @@
       <router-link to="/" v-slot="{ href, navigate }">
         <v-toolbar-title @click="navigate" :href="href" style="cursor:pointer">Vue challenges</v-toolbar-title>
       </router-link>
+      <v-spacer></v-spacer>
+      <div class="text-center">
+        <v-menu offset-y>
+          <template v-slot:activator="{ on }">
+            <v-btn class="teal lighten-2" v-on="on">Menu</v-btn>
+          </template>
+          <v-list>
+            <router-link to="/components" v-slot="{ href, navigate }">
+              <v-list-item @click="navigate" :href="href">
+                <v-list-item-title>Components</v-list-item-title>
+              </v-list-item>
+            </router-link>
+            <router-link to="/filters" v-slot="{ href, navigate }">
+              <v-list-item @click="navigate" :href="href">
+                <v-list-item-title>Filters</v-list-item-title>
+              </v-list-item>
+            </router-link>
+          </v-list>
+        </v-menu>
+      </div>
     </v-app-bar>
     <v-content>
       <router-view />
